e0440bb68d7ce3e8c5f1dfb729929d1f7cf8fc28
Ali <alisinai902@gmail.com>
2025-02-22 23:38:43 +0430

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,95 @@
-import Image from "next/image";
+"use client";
+import { useState, useEffect } from "react";
 
-export default function Home() {
+const API_KEY = "AIzaSyCC5V7uIYXRkcl36YzQOpPWydclmfbMHIU";
+
+export default function HomePage() {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideoId, setSelectedVideoId] = useState<string | null>(null);
+  const [query, setQuery] = useState("");
+
+  async function fetchTrendingVideos() {
+    const res = await fetch(
+      `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&regionCode=US&maxResults=10&key=${API_KEY}`
+    );
+    if (!res.ok) {
+      console.error("Failed to fetch YouTube videos");
+      return;
+    }
+    const data = await res.json();
+    setVideos(data.items);
+    setSelectedVideoId(data.items[0]?.id); 
+  }
+
+  async function searchVideos() {
+    if (!query) return fetchTrendingVideos(); 
+
+    const res = await fetch(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&type=video&maxResults=6&key=${API_KEY}`
+    );
+    if (!res.ok) {
+      console.error("Failed to fetch search results");
+      return;
+    }
+    const data = await res.json();
+    setVideos(data.items);
+    setSelectedVideoId(data.items[0]?.id);
+  }
+
+  useEffect(() => {
+    fetchTrendingVideos();
+  }, []);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="py-2 bg-white flex flex-col px-3 ">
+      <div className=" relative w-full px-5  py-3 shadow-lg shadow-gray-400">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <input
+      type="text"
+      placeholder=""
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+      onKeyDown={(e) => e.key === 'Enter' && searchVideos()}
+      className=" peer py-4 w-full 
+      pr-36 border border-gray-700 px-4 rounded  focus:border-blue-500 text-black focus:outline-none focus:border-2"/>
+      <label
+      htmlFor="input"
+      className="  bg-white absolute left-9 mt-5  text-sm transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-900 peer-focus:-mt-2 peer-focus:text-sm peer-focus:text-blue-500 ">
+        Search...
+      </label>
+      </div>
+
+      <div className=" flex  flex-col justify-center items-center md:grid md:grid-cols-2">
+        <div className=" flex w-full justify-center ]">{selectedVideoId && (
+            <iframe
+            className=" w-full max-w-[600px]  aspect-video rounded  shadow-lg max-sm:mt-10  "
+            src={`https://www.youtube.com/embed/${selectedVideoId}`}
+            allowFullScreen ></iframe>
+            
+          
+        )}
+        
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="grid grid-cols-2 max-sm:gap-4 gap-2 items-center justify-center text-center ">
+        {videos.slice(0 , 4).map((video: any) => (
+          <div
+            key={video.id.videoId ||video.id}
+            className="p-4  border rounded shadow cursor-pointer hover:bg-gray-100 transition flex flex-col justify-center items-center "
+            onClick={() => setSelectedVideoId(video.id.videoId|| video.id)} // انتخاب ویدیو
+          >
+            
+            <img
+              src={video.snippet.thumbnails.medium.url}
+              alt={video.snippet.title}
+              className="rounded mt-2"
+            />
+            <h2 className="text-blue-500 font-semibold max-w-[300px]">{video.snippet.title}</h2>
+          </div>
+        ))}
+        </div>
+      </div>
+      
     </div>
   );
-}
+};
+
